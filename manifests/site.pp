1f0d7ae9801b2e0c4c35a8cac02db902ee4fe600
Maksym Sinichenko <maksym_sinichenko@epam.com>
2018-12-14 15:09:00 +0200

    Change site.pp to use regexp

--- manifests/site.pp
+++ manifests/site.pp
@@ -24,14 +24,40 @@ File { backup => false }
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-node 'tthomsen1.mylabserver.com' {
-  include pe_repo::platform::el_6_x86_64
-  include pe_repo::platform::ubuntu_1204_amd64
+
+#$test_top_scope = "Top Scope"                                                                                         │
+                                                                                                                      │
+node 'puppet-agent-test-ubuntu.kyiv.epam.com'{                                                                        │
+ notify { "This matches a node name!!!!!!!!!!!!! !": }                                                                │
+ #class { 'java': }                                                                                                    │
+}                                                                                                                     │
+node /^puppet-agent-test-ubuntu/ {                                                                                    │
+  notify { "This is a node def with regX !": }                                                                        │
+}
+node 'puppet-agent-test.kyiv.epam.com' {
+}│
+node default {                                                                                                        │
+ # $test_top_scope = "This node scope"                                                                                 │
+ # $node_scope_var = "Node scop"                                                                                       │
+ # notify {"test_scope":                                                                                               │
+ #  message => "This is a top scope var: ${test_top_scope}"                                                           │
+ # }                                                                                                                   │
+ # notify { "This is default node !": }                                                                                │
+ # class { 'java': }                                                                                                   │
+#  include ntp                                                                                                        │
+  # This is where you can declare classes for all nodes.                                                              │
+  # Example:                                                                                                          │
+  #   class { 'my_class': }                                                                                           │
 }
 
-node default {
-  hiera_include('classes')
+#node 'tthomsen1.mylabserver.com' {
+#  include pe_repo::platform::el_6_x86_64
+# include pe_repo::platform::ubuntu_1204_amd64
+#}
+
+#node default {
+#  hiera_include('classes')
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-}
+#}
